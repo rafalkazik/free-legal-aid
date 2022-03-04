4c2516426618b9afeff09592ae23e04e9f58332b
Rafał Kazik <kazik.raf@gmail.com>
2022-03-04 19:58:50 +0100

    rwd project

--- src/components/OrgBoard.js
+++ src/components/OrgBoard.js
@@ -13,34 +13,50 @@ function OrgBoard({ data, inputValue }) {
   const showOrgs = filterData.map((org) => {
     return (
       <li key={org.id} className='org-list__org-item org-item'>
-        <div className='org-item__org-name'>
-          <h3 className='org-item__content org-item__org-name-text'>
-            {org.orgName}
-          </h3>
-        </div>
-        <div className='org-item__city-content city-content'>
-          <div className='city-content__icon'>
-            <LocationOnOutlinedIcon />
+        <header className='org-item__header-wrapper'>
+          <div className='org-item__org-name'>
+            <h3 className='org-item__content org-item__org-name-text'>
+              {org.orgName}
+            </h3>
           </div>
-          <div className='city-content__city-name city-name'>
-            <p className='city-name__text'>{org.city}</p>
+          <div className='org-item__city-content city-content'>
+            <div className='city-content__icon'>
+              <LocationOnOutlinedIcon />
+            </div>
+            <div className='city-content__city-name city-name'>
+              <p className='city-name__text'>{org.city}</p>
+            </div>
           </div>
-        </div>
+        </header>
         <div className='org-item__org-data-content'>
-          <p className='org-item__content--title org-item__org-adress'>
-            adress:
-          </p>
-          <p className='org-item__content org-item__org-adress'>{org.adress}</p>
-          <p className='org-item__content--title org-item__org-email'>email:</p>
-          <p className='org-item__content org-item__org-email'>{org.email}</p>
-          <p className='org-item__content--title org-item__org-phone'>phone:</p>
-          <p className='org-item__content org-item__org-phone'>{org.phone}</p>
-          <p className='org-item__content--title org-item__org-coordinator'>
-            coordinator:
-          </p>
-          <p className='org-item__content org-item__org-coordinator'>
-            {org.coordinator}
-          </p>
+          <div className='org-data-content-wrapper__adress'>
+            <p className='org-item__content--title org-item__org-adress'>
+              adress:
+            </p>
+            <p className='org-item__content org-item__org-adress'>
+              {org.adress}
+            </p>
+          </div>
+          <div className='org-data-content-wrapper__email'>
+            <p className='org-item__content--title org-item__org-email'>
+              email:
+            </p>
+            <p className='org-item__content org-item__org-email'>{org.email}</p>
+          </div>
+          <div className='org-data-content-wrapper__phone'>
+            <p className='org-item__content--title org-item__org-phone'>
+              phone:
+            </p>
+            <p className='org-item__content org-item__org-phone'>{org.phone}</p>
+          </div>
+          <div className='org-data-content-wrapper__coordinator'>
+            <p className='org-item__content--title org-item__org-coordinator'>
+              coordinator:
+            </p>
+            <p className='org-item__content org-item__org-coordinator'>
+              {org.coordinator}
+            </p>
+          </div>
         </div>
       </li>
     );
